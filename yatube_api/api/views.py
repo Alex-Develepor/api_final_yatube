4838e64f919ccd6c21f2767f18a43e31be31e6c5
Дворецкий Александр <Ub1ka@yandex.ru>
2022-05-23 18:41:02 +0300

    Var4

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from posts.models import Group, Post
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
+from posts.models import Group, Post
 from .permission import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
